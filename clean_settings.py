a85ad1f9791a61c257797cf0e62465fac0fd43dc
theoikonomou <theo.ikonomou@artirix.com>
2013-12-04 11:57:32 +0000

    support virtual envs flake8

--- clean_settings.py
+++ clean_settings.py
@@ -1,11 +1,14 @@
-#!usr/bin/python
+#!usr/bin/env python
+
 import sys
 import os.path
 import re
 import fileinput
 
-
-SETTINGS_PATH = os.path.abspath(sys.argv[1]) if len(sys.argv) > 1 else 'settings.py'
+if len(sys.argv) > 1:
+    SETTINGS_PATH = os.path.abspath(sys.argv[1])
+else:
+    SETTINGS_PATH = 'settings.py'
 
 for line in fileinput.input(SETTINGS_PATH, inplace=1):
     line = re.sub(r'#\s.*$', '', line)
@@ -15,8 +18,8 @@ for line in fileinput.input(SETTINGS_PATH, inplace=1):
     if line.rstrip():
         print line,
 
-pattern1 = re.compile('[A-Z]|\(')
-pattern2 = re.compile('\)|\}')
+pattern1 = re.compile(r'[A-Z]|\(')
+pattern2 = re.compile(r'\)|\}')
 for line in fileinput.input(SETTINGS_PATH, inplace=1):
     if pattern1.match(line):
         print line
